7e593a9546903f286942f9ee3b7e3ecdda992534
paintover <tjdus1139@naver.com>
2024-05-11 10:00:31 +0900

    로그인 상태유지 체크박스 만들기

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   port: 8086,
-  root: "src/",
-  open: ["index.html"],
+  root: "naver/",
+  // src/ 상태에서 계속 404 error가 나서 root를 naver/로 바꾸니까 되긴 된다. 
+  // open: ["index.html"],
   host: "localhost",
   browser: "chrome",
   https: false,
